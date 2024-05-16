46d38c77c95c9ab1786b240152fb97a238b9b3ec
azibomfim <bomfim@discente.ufg.br>
2024-05-16 08:17:07 -0300

    adicionada descrição no enum Arma

--- Coders Growth/CodersGrowth.Dominio/Enums/ArmaEnum.cs
+++ Coders Growth/CodersGrowth.Dominio/Enums/ArmaEnum.cs
@@ -8,10 +8,15 @@ namespace CodersGrowth.Dominio.Enums
 {
     public enum ArmaEnum
     {
+        [Description("Espada")]
         Espada,
+        [Description("Espadão")]
         Espadao,
+        [Description("Catalisador")]
         Catalisador,
+        [Description("Arco")]
         Arco,
+        [Description("Lança")]
         Lanca
     }
 }
