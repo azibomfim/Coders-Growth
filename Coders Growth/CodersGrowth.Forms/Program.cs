8fba0dabfd241603fd6257859bb4efbba90ebea1
azibomfim <bomfim@discente.ufg.br>
2024-06-26 11:43:36 -0300

    (configuracao-do-projeto-com-injecao) configurado o projeto com injeção #time 0h 0m

--- Coders Growth/CodersGrowth.Forms/Program.cs
+++ Coders Growth/CodersGrowth.Forms/Program.cs
@@ -1,59 +1,71 @@
 ﻿using CodersGrowth.Dominio.Interfaces;
 using CodersGrowth.Dominio.Migracoes;
-using CodersGrowth.Infra;
 using CodersGrowth.Infra.Repositorios;
 using CodersGrowth.Servicos.Servicos;
 using CodersGrowth.Servicos.Validacoes;
 using FluentMigrator.Runner;
 using FluentValidation;
-using LinqToDB;
-using LinqToDB.AspNet;
 using LinqToDB.AspNet.Logging;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using LinqToDB;
+using CodersGrowth.Infra;
+using LinqToDB.AspNet;
 using System.Configuration;
 
-namespace CodersGrowth.Forms
-{
-    class Program
-    {
-        private static string _stringDeConexao = "GenshinLibraryDB";
-        static void Main(string[] args)
-        {
-            using (var serviceProvider = CriarServicos())
-            using (var scope = serviceProvider.CreateScope())
-            {
-                AtualizarBancoDeDados(scope.ServiceProvider);
-            }
-        }
+namespace CodersGrowth.Forms;
 
-        private static ServiceProvider CriarServicos()
-        {
-            return new ServiceCollection()
-                .AddFluentMigratorCore()
-                .ConfigureRunner(rb => rb
-                .AddSqlServer()
-                .WithGlobalConnectionString("Data Source=DESKTOP-F7MOBD8\\SQLEXPRESS;Initial Catalog=CodersGrowth;User ID=sa;Password=sap@123;Trust Server Certificate=True")
-                .ScanIn(typeof(_2024062115290000).Assembly).For.Migrations())
-                .AddScoped<ServicoPersonagem>()
-                .AddScoped<ServicoUsuario>()
-                .AddScoped<IRepositorioPersonagem, RepositorioPersonagem>()
-                .AddScoped<IRepositorioUsuario, RepositorioUsuario>()
-                .AddScoped<IValidator, ValidacaoPersonagem>()
-                .AddScoped<IValidator, ValidacaoUsuario>()
-                .AddLinqToDBContext<ConexaoDados>((provider, options)
+internal static class Program
+{
+    private static string _stringDeConexao = "GenshinLibraryDB";
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        var host = CriarHostBuilder().Build();
+        var ServiceProvider = host.Services;
+
+        Application.Run(ServiceProvider.GetRequiredService<Form1>());
+    }
+
+    static IHostBuilder CriarHostBuilder()
+    {
+        return Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                services.AddScoped<ServicoPersonagem>()
+               .AddScoped<ServicoUsuario>()
+               .AddScoped<IRepositorioPersonagem, RepositorioPersonagem>()
+               .AddScoped<IRepositorioUsuario, RepositorioUsuario>()
+               .AddScoped<IValidator, ValidacaoPersonagem>()
+               .AddScoped<IValidator, ValidacaoUsuario>()
+               .AddScoped<Form1>();
+            });
+    }
+
+    private static ServiceProvider CriarServicos()
+    {
+        return new ServiceCollection()
+        .AddFluentMigratorCore()
+        .ConfigureRunner(rb => rb
+            .AddSqlServer()
+            .WithGlobalConnectionString("Data Source=DESKTOP-F7MOBD8\\SQLEXPRESS;Initial Catalog=CodersGrowth;User ID=sa;Password=sap@123;Trust Server Certificate=True")
+            .ScanIn(typeof(_2024062115290000).Assembly).For.Migrations()
+            .ScanIn(typeof(_2024062409440000).Assembly).For.Migrations())
+         .AddLinqToDBContext<ConexaoDados>((provider, options)
                 => options
                 .UseSqlServer(ConfigurationManager
                 .ConnectionStrings[_stringDeConexao].ConnectionString)
                 .UseDefaultLogging(provider))
                 .AddLogging(lb => lb.AddFluentMigratorConsole())
-                .BuildServiceProvider(false);
-        }
+         .BuildServiceProvider(false); } 
+    
 
-        private static void AtualizarBancoDeDados(IServiceProvider serviceProvider)
-        {
-            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+    private static void AtualizarBancoDeDados(IServiceProvider serviceProvider)
+    {
+        var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
 
-            runner.MigrateUp();
-        }
+        runner.MigrateUp();
     }
+
 }
