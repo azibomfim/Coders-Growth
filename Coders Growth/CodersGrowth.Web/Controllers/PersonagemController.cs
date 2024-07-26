b3319f9e801069c190fbb9adad6746f91c641d44
azibomfim <bomfim@discente.ufg.br>
2024-07-26 10:13:37 -0300

    (configurar-o-projeto-dotnet-como-api-com-injecao-de-dependencia) configurado projeto api com injeção de dependencia

---
+++ Coders Growth/CodersGrowth.Web/Controllers/PersonagemController.cs
@@ -0,0 +1,56 @@
+﻿using CodersGrowth.Dominio.Filtros;
+using CodersGrowth.Dominio.Models;
+using CodersGrowth.Servicos.Servicos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodersGrowth.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonagemController : ControllerBase
+    {
+        private readonly ServicoPersonagem _servicoPersonagem;
+
+        public PersonagemController (ServicoPersonagem servicoPersonagem)
+        {
+            _servicoPersonagem = servicoPersonagem;
+        }
+
+        [HttpGet]
+        public IActionResult ObterTodos([FromQuery]FiltroPersonagem? filtroPersonagem)
+        {
+            var personagens = _servicoPersonagem.ObterTodos(filtroPersonagem);
+            return Ok(personagens);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId([FromRoute]int id)
+        {
+            var personagem = _servicoPersonagem.ObterPorId(id);
+            return Ok(personagem);
+        }
+
+        [HttpPost]
+        public IActionResult Criar([FromBody] Personagem personagemCriar)
+        {
+            _servicoPersonagem.Criar(personagemCriar);
+            return CreatedAtAction(nameof(ObterPorId), new { id = personagemCriar.Id }, personagemCriar);
+        }
+
+        [HttpPut]
+        public IActionResult Editar([FromBody] Personagem personagemEditar)
+        {
+            _servicoPersonagem.Editar(personagemEditar);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remover([FromRoute]int id)
+        {
+            _servicoPersonagem.Remover(id);
+            return NoContent();
+        }
+    }
+}
+
+
---
+++ Coders Growth/CodersGrowth.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,54 @@
+﻿using CodersGrowth.Dominio.Filtros;
+using CodersGrowth.Dominio.Models;
+using CodersGrowth.Servicos.Servicos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodersGrowth.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly ServicoUsuario _servicoUsuario;
+
+        public UsuarioController(ServicoUsuario servicoUsuario)
+        {
+            _servicoUsuario = servicoUsuario;
+        }
+
+        [HttpGet]
+        public IActionResult ObterTodos([FromQuery] FiltroUsuario? filtroUsuario)
+        {
+            var usuarios = _servicoUsuario.ObterTodos(filtroUsuario);
+            return Ok(usuarios);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var usuario = _servicoUsuario.ObterPorId(id);
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Criar([FromBody] Usuario usuarioCriar)
+        {
+            _servicoUsuario.Criar(usuarioCriar);
+            return CreatedAtAction(nameof(ObterPorId), new { id = usuarioCriar.Id }, usuarioCriar);
+        }
+
+        [HttpPut]
+        public IActionResult Editar([FromBody] Usuario usuarioEditar)
+        {
+            _servicoUsuario.Editar(usuarioEditar);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remover(int id)
+        {
+            _servicoUsuario.Remover(id);
+            return NoContent();
+        }
+    }
+}
